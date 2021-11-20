a4448122f69eff148252080c9bf7f2ce41144677
Bram Diederik <bram.diederik@gmail.com>
2021-11-20 14:40:38 +0100

    added loging information for vpnadmin to the openvpn log.

--- VpnGateway/bin/vpnadmin.sh
+++ VpnGateway/bin/vpnadmin.sh
@@ -14,7 +14,7 @@ then
 
 	for f in /etc/openvpn/clientConfig/*;
    	 do 
-          conf=`echo $f | grep -E "\w+\.conf" -o`;
+          conf=`echo $f | grep -E "[A-Za-z0-9_\-]+\.conf" -o`;
           if [ "$link" = "$f" ];
 	   then
              echo "* $conf";
@@ -36,16 +36,19 @@ if [ "$1" = "start" ];
   then
     if [ -z "$2" ];
     then
-      service openvpn start
+        date +"%b %d %T `hostname` vpnadmin: openvpn start $2" >> /var/log/openvpn/ovpn.log
+	service openvpn start
     else
       link=`readlink /etc/openvpn/client.conf`
       for f in /etc/openvpn/clientConfig/*;
          do 
-           conf=`echo $f | grep -E "\w+\.conf" -o`; 
+           conf=`echo $f | grep -E "[A-Za-z0-9_\-]+\.conf" -o`; 
            if [ "$conf" = $2 ];
            then
              ln -sf $f /etc/openvpn/client.conf
              service openvpn restart
+             sysctl -w net.ipv4.ip_forward=1 
+             iptables -t nat -A POSTROUTING ! -o lo -j MASQUERADE
            fi
           done
      fi
@@ -54,13 +57,17 @@ if [ "$1" = "stop" ];
   then
     if [ -z "$2" ];
     then 
+       date +"%b %d %T `hostname` vpnadmin: Forwaring stopped" >> /var/log/openvpn/ovpn.log
        service openvpn stop
        sysctl -w net.ipv4.ip_forward=0
+       iptables -t nat -D POSTROUTING ! -o lo -j MASQUERADE
     else
       if [ "route-local" = $2 ];
            then
-             service openvpn stop
+             date +"%b %d %T `hostname` vpnadmin: Local routing started" >> /var/log/openvpn/ovpn.log
+	     service openvpn stop
              sysctl -w net.ipv4.ip_forward=1 
+             iptables -t nat -A POSTROUTING ! -o lo -j MASQUERADE
            fi
      fi
 fi
