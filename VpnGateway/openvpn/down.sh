b4a364db02e166f877d371008d3edcf5a2c05593
Bram Diederik <bram.diederik@gmail.com>
2021-11-20 14:21:25 +0100

    moved up and down inteligence to vpnadmin script. So that the local routing works directly

--- VpnGateway/openvpn/down.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-iptables -t nat -D POSTROUTING ! -o lo -j MASQUERADE
-sysctl -w net.ipv4.ip_forward=0
-exit 0
--- VpnGateway/openvpn/up.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sysctl -w net.ipv4.ip_forward=1 
-iptables -t nat -A POSTROUTING ! -o lo -j MASQUERADE
-exit 0
