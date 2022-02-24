67672e26b847db4fc1e66967ef49b49146b52c2c
Bram Diederik <bram.diederik@gmail.com>
2022-02-24 21:45:22 +0100

    better up script

--- incomingVpnServer/openvpn/up.sh
+++ incomingVpnServer/openvpn/up.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
-iptables -A FORWARD -i tun0 -j ACCEPT 
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE 
-ip route add 10.8.0.0/24 dev tun0 table 11
-ip route add default via 192.168.112.4 dev eth0 table 11
+IF=eth0
+TUN=tun0
+VPN_GATEWAY=192.168.112.4
+
+## Enable Forwarding for the TUN interface
+sysctl -w net.ipv4.ip_forward=1
+iptables -A FORWARD -i $TUN -j ACCEPT 
+iptables -t nat -A POSTROUTING -o $IF -j MASQUERADE 
+
+## Create seperate IP routing table for the TUN Interface
 ip rule add from 10.8.0.0/24 table 11
 ip rule add to 10.8.0.0/24 table 11
+ip route add 10.8.0.0/24 dev $TUN table 11
+## Make the VPN Gateway the default gateway
+ip route add default via $VPN_GATEWAY dev $IF table 11
+## Uncomment and edit your local network ranges 
+## or else you have no local network and the whole setup is pointless
+#ip route add <localIPRange>  dev <interface> table 11
 exit 0
