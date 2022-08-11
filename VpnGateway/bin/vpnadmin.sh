da4232796f4c505be4e748a609e26b7741b60e8b
Bram Diederik <bram.diederik@gmail.com>
2022-08-11 23:19:11 +0200

    added help and integration with https://github.com/tobias-kuendig/hacompanion

--- VpnGateway/bin/vpnadmin.sh
+++ VpnGateway/bin/vpnadmin.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 ipenable() {
    if grep -q 0 /proc/sys/net/ipv4/ip_forward;
@@ -16,11 +16,19 @@ ipdisable() {
         iptables -t nat -D POSTROUTING ! -o lo -j MASQUERADE;
    fi
 }
-
+if [ "$1" = "help" ]; 
+then
+  echo "general help of a custom script to switch between vpn gateways"
+  echo "ls : list connections and show current used"
+  echo "status : shows the status"
+  echo "start : [connection] starts a connection"
+  echo "stop : [route-local] stops or stops and start local routing"
+  echo "NULL : integration with https://github.com/tobias-kuendig/hacompanion#custom-scripts that does not accept a argument"
+fi
 
 if [ "$1" = "ls" ]; 
 then 
-   status=`systemctl show openvpn@client --no-page)` 
+   status=`systemctl show openvpn@client --no-page` 
 
    status_text=`echo "$status" | grep 'ActiveState='`
    if [ "$status_text" = "ActiveState=active" ];
@@ -41,14 +49,34 @@ then
           fi
 	done
 fi
-if [ "$1" = "status" ]; 
+if [ "$1" = "status" ]
 then 
-   status=`systemctl show openvpn@client --no-page)` 
+   status=`systemctl show openvpn@client --no-page` 
 
 #echo $status
    status_text=`echo "$status" | grep 'ActiveState='` 
    echo $status_text
    sysctl net.ipv4.ip_forward
+   if [ "$status_text" = "ActiveState=active" ];
+      then
+        link=`readlink /etc/openvpn/client.conf`
+      else
+       link="null"
+      fi
+
+   for f in /etc/openvpn/clientConfig/*;
+   do 
+     conf=`echo $f | grep -E "[A-Za-z0-9_\-]+\.conf" -o`;
+     if [ "$link" = "$f" ];
+       then
+          conf2=${conf::-5}
+           #conf=`echo $conf | cut -c 5`;
+           echo "$conf2";
+       break
+     fi
+   done
+
+
 fi
 if [ "$1" = "start" ]; 
   then
@@ -86,4 +114,35 @@ if [ "$1" = "stop" ];
              ipenable
            fi
      fi
+fi
+
+if [ ! "$1" ]
+then 
+   status=`systemctl show openvpn@client --no-page` 
+
+   status_text=`echo "$status" | grep 'ActiveState='` 
+#   echo $status_text
+#   sysctl net.ipv4.ip_forward
+   if [ "$status_text" = "ActiveState=active" ];
+      then
+        link=`readlink /etc/openvpn/client.conf`
+        for f in /etc/openvpn/clientConfig/*;
+        do 
+          conf=`echo $f | grep -E "[A-Za-z0-9_\-]+\.conf" -o`;
+          if [ "$link" = "$f" ];
+            then
+            conf2=${conf::-5}
+            #conf=`echo $conf | cut -c 5`;
+            echo "$conf2";
+            echo "icon:mdi:lan-connect"
+	    ip=`curl icanhazip.com 2> /dev/null`
+            echo "ip_address:$ip"
+            break
+          fi
+        done
+   else 
+      echo down
+      echo "icon:mdi:lan-disconnect"
+      echo ip_address:null
+   fi
 fi
