6f2a2d110e36c14baeeb05b36b07ae33dc867998
Bram Diederik <bram.diederik@gmail.com>
2022-11-03 23:22:26 +0100

    better home assistant integration

--- VpnGateway/bin/vpnadmin.sh
+++ VpnGateway/bin/vpnadmin.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+tun=$tun0;
 ipenable() {
    if grep -q 0 /proc/sys/net/ipv4/ip_forward;
      then
@@ -8,6 +8,18 @@ ipenable() {
    fi
 }
 
+list_connections() {
+list="[\"stop\",\"stop-route-local\"";
+ 
+for f in /etc/openvpn/clientConfig/*;
+  do 
+    conf=`echo $f | grep -E "([A-Za-z0-9_\-]+)\.conf" -o`;
+    conf2=${conf::-5}
+    list="${list},\"${conf2}\"";
+  done
+    list="${list}]";
+echo connections:$list
+}
 
 ipdisable() {
    if grep -q 1 /proc/sys/net/ipv4/ip_forward;
@@ -80,7 +92,17 @@ then
 fi
 if [ "$1" = "start" ]; 
   then
-    if [ -z "$2" ];
+  if [ "$2" = "stop" ]; 
+  then
+     date +"%b %d %T `hostname` vpnadmin: Forwaring stopped" >> /var/log/openvpn/ovpn.log
+     service openvpn stop
+     ipdisable
+   elif [ "stop-route-local" = $2 ];
+   then
+      date +"%b %d %T `hostname` vpnadmin: Local routing started" >> /var/log/openvpn/ovpn.log
+      service openvpn stop
+      ipenable
+    elif [ -z "$2" ];
     then
         date +"%b %d %T `hostname` vpnadmin: openvpn start $2" >> /var/log/openvpn/ovpn.log
 	service openvpn start
@@ -91,6 +113,11 @@ if [ "$1" = "start" ];
          do 
            conf=`echo $f | grep -E "[A-Za-z0-9_\-]+\.conf" -o`; 
            if [ "$conf" = $2 ];
+           then
+             ln -sf $f /etc/openvpn/client.conf
+             service openvpn restart
+             ipenable
+           elif  [ $conf = "${2}.conf" ];
            then
              ln -sf $f /etc/openvpn/client.conf
              service openvpn restart
@@ -131,18 +158,32 @@ then
           conf=`echo $f | grep -E "[A-Za-z0-9_\-]+\.conf" -o`;
           if [ "$link" = "$f" ];
             then
-            conf2=${conf::-5}
-            #conf=`echo $conf | cut -c 5`;
-            echo "$conf2";
-            echo "icon:mdi:lan-connect"
-	    ip=`curl icanhazip.com 2> /dev/null`
-            echo "ip_address:$ip"
-            break
+            
+            if [ " ping -c 1 `ip -4 addr show $tun | grep -oP '(?<=inet\s)\d+(\.\d+){3}'` " ];
+            then 
+              conf2=${conf::-5}
+              echo "true"
+              echo "location:$conf2";
+              echo "icon:mdi:lan-connect"
+	      ip=`curl icanhazip.com 2> /dev/null`
+              echo "ip_address:$ip"
+	      list_connections
+              break
+           else 
+            echo "false"
+            echo "location:null"
+            echo "icon:mdi:lan-disconnect"
+            echo ip_address:null
+            list_connections
+           fi
           fi
         done
-   else 
-      echo down
+   else
+      echo "false"
+      echo "location:null"
       echo "icon:mdi:lan-disconnect"
       echo ip_address:null
+      list_connections
    fi
 fi
+
